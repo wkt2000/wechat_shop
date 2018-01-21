a6e304ec4fbbc90fde44b6b2040e1490447cd270
tail <tailgecko@outlook.com>
2018-01-21 22:23:43 +0800

    gw

--- get_access_token.php
+++ get_access_token.php
@@ -11,7 +11,7 @@ $url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&app
 //初始化
 $ad = curl_init();
 //设置选项，包括URL
-curl_setopt($ad, CURLOPT_URL, "http://www.jb51.net");
+curl_setopt($ad, CURLOPT_URL, $url);
 curl_setopt($ad, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ad, CURLOPT_HEADER, 0);
 //执行并获取HTML文档内容
@@ -19,5 +19,5 @@ $output = curl_exec($ad);
 //释放curl句柄
 curl_close($ad);
 //写入获得的数据
-//file_put_contents("./access_token.txt",$output);
+file_put_contents("./access_token.txt",$output);
 print_r($output);
